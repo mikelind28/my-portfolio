05e9f01ad498d4876ffdd5d2450fedc583ee25dd
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-01-25 17:07:47 -0600

    add header icons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,28 @@
+import { TabGroup, TabList, Tab } from "@headlessui/react";
+import { IoMenuOutline, IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
+
+function DarkModeToggle() {
+    return (
+        <TabGroup>
+            <TabList className="flex *:rounded-full *:px-2 *:py-1 *:text-xl *:font-semibold *:text-white *:focus:not-data-focus:outline-none *:data-focus:outline *:data-focus:outline-white *:data-hover:bg-white/5 *:data-selected:bg-white/10 *:data-selected:data-hover:bg-white/10">
+                <Tab>
+                    <IoMoonOutline />
+                </Tab>
+
+                <Tab>
+                    <IoSunnyOutline />
+                </Tab>
+            </TabList>
+        </TabGroup>
+    );
+}
+
 export default function Header() {
     return (
-        <header className="group p-4 flex justify-around">
-            <h1 className="text-2xl font-extralight tracking-wider *:transition-colors *:duration-700 *:ease-in-out">
+        <header className="p-4 flex justify-between items-center">
+            <IoMenuOutline className="mr-2 text-white text-2xl" />
+
+            <h1 className="group text-2xl font-extralight tracking-wider *:transition-colors *:duration-700 *:ease-in-out">
                 <span className="bg-clip-text text-transparent bg-linear-to-tl from-orange-500 to-orange-400 group-hover:bg-linear-to-tr group-hover:from-orange-500 group-hover:to-orange-300">
                     mike lind
                 </span>
@@ -14,6 +35,8 @@ export default function Header() {
                     web dev
                 </span>
             </h1>
+
+            <DarkModeToggle />
         </header>
     );
 }
